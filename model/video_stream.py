4aee33a6a1e2c8b25de36fb49d82ee68d5055d95
jakmol@ug.kth.se <MmR-PZ-Q>
2019-04-09 14:26:13 +0200

    restricted access to stream with ip

---
+++ model/video_stream.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, Response, request, abort
+from camera_pi import Camera
+import os
+import sys
+FILE_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(f"{FILE_DIR}/../integration")
+
+def start_stream(client_ip,port):
+    PORT = port
+    SERVER_IP = '130.237.215.167'
+
+    app = Flask(__name__)
+    @app.before_request
+    def limit_remote_addr():
+            if request.remote_addr != client_ip:
+                abort(403)
+    @app.route('/')
+        # def index():
+        #     """Video streaming home page."""
+        #     return render_template('index.html')
+    def gen(camera):
+        """Video streaming generator function."""
+    while True:
+            frame = camera.get_frame()
+            yield (b'--frame\r\n'+b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+            @app.route('/video_feed')
+            def video_feed():
+                """Video streaming route. Put this in the src attribute of an img tag."""
+                return Response(gen(Camera()), mimetype='multipart/x-mixed-replace; boundary=frame')
+            if __name__ == '__main__':
+                app.run(host=SERVER_IP, port = PORT, debug=True, threaded=True)
