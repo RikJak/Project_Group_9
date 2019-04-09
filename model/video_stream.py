84cc2c65d2397c5da78b668f640d1ceb08e71ff5
jakmol@ug.kth.se <MmR-PZ-Q>
2019-04-09 15:32:19 +0200

    video streaming fixing

--- model/video_stream.py
+++ model/video_stream.py
@@ -8,26 +8,26 @@ from camera_pi import Camera
 
 
 # def start_stream(client_ip,port):
-    PORT = port
-    SERVER_IP = '130.237.215.167'
-    app = Flask(__name__)
+PORT = 8000
+SERVER_IP = '130.237.215.167'
+app = Flask(__name__)
     # app.run(host=SERVER_IP, port = PORT, debug=True, threaded=True)
     # @app.before_request
     # def limit_remote_addr():
     #         if request.remote_addr != client_ip:
     #             abort(403)
-    @app.route('/')
+@app.route('/')
         # def index():
         #     """Video streaming home page."""
         #     return render_template('index.html')
-    def gen(camera):
+def gen(camera):
         """Video streaming generator function."""
-        while True:
-            frame = camera.get_frame()
-            yield (b'--frame\r\n'+b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-    @app.route('/video_feed')
-    def video_feed():
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'+b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+@app.route('/video_feed')
+def video_feed():
             """Video streaming route. Put this in the src attribute of an img tag."""
-            return Response(gen(Camera()), mimetype='multipart/x-mixed-replace; boundary=frame')
-    if __name__ == '__main__':
-      app.run(host=SERVER_IP, port = PORT, debug=True, threaded=True)
+        return Response(gen(Camera()), mimetype='multipart/x-mixed-replace; boundary=frame')
+if __name__ == '__main__':
+  app.run(host=SERVER_IP, port = PORT, debug=True, threaded=True)
