0073f63aa10f88fb197e7f65102f5f4d2d54e5b5
jakmol@ug.kth.se <MmR-PZ-Q>
2019-04-09 14:30:57 +0200

    restricted access to stream with ip

--- view/view.py
+++ view/view.py
@@ -9,7 +9,7 @@ from contr import Controller
 from flask import Flask, redirect, url_for, request
 app = Flask(__name__)
 controller = Controller()
-@app.route('/view',methods = ['POST'])
+@app.route('/',methods = ['POST'])
 def request_handler(self):
    if self.request.method == 'POST':
       print("found")
