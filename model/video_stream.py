b0706b4becd3a6ffa6b1e28a1037d38672eb2f4b
jakmol@ug.kth.se <MmR-PZ-Q>
2019-04-09 15:33:12 +0200

    video streaming fixing

--- model/video_stream.py
+++ model/video_stream.py
@@ -21,13 +21,13 @@ app = Flask(__name__)
         #     """Video streaming home page."""
         #     return render_template('index.html')
 def gen(camera):
-        """Video streaming generator function."""
+    """Video streaming generator function."""
     while True:
         frame = camera.get_frame()
         yield (b'--frame\r\n'+b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 @app.route('/video_feed')
 def video_feed():
-            """Video streaming route. Put this in the src attribute of an img tag."""
+        """Video streaming route. Put this in the src attribute of an img tag."""
         return Response(gen(Camera()), mimetype='multipart/x-mixed-replace; boundary=frame')
 if __name__ == '__main__':
   app.run(host=SERVER_IP, port = PORT, debug=True, threaded=True)
