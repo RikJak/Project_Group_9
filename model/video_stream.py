366ec12c6decb294eac4d7279db02b9c611f0272
jakmol@ug.kth.se <MmR-PZ-Q>
2019-04-09 21:21:19 +0200

    testing variable send

--- model/video_stream.py
+++ model/video_stream.py
@@ -12,9 +12,6 @@ from flask import Flask, render_template, Response
 from camera_pi import Camera
 print(sys.argv)
 print(len(sys.argv))
-arguments = sys.argv
-print(arguments[0])
-print(arguments[1])
 # client_ip = sys.argv[1]
 # port = sys.argv[2]
 
