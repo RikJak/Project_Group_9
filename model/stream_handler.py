32e4f1f7feac290f29f95b16ede3af8f0fbff1b1
jakmol@ug.kth.se <MmR-PZ-Q>
2019-04-09 13:20:15 +0200

    h

--- model/stream_handler.py
+++ model/stream_handler.py
@@ -21,16 +21,16 @@ PORT = 8001
 
 app = Flask(__name__)
 @app.route('/')
-def index():
-    """Video streaming home page."""
-    return render_template('index.html')
-def gen(camera):
-    """Video streaming generator function."""
-    while True:
-        frame = camera.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-@app.route('/video_feed')
+# def index():
+#     """Video streaming home page."""
+#     return render_template('index.html')
+# def gen(camera):
+#     """Video streaming generator function."""
+#     while True:
+#         frame = camera.get_frame()
+#         yield (b'--frame\r\n'
+#                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+# @app.route('/video_feed')
 def video_feed():
     """Video streaming route. Put this in the src attribute of an img tag."""
     return Response(gen(Camera()),
