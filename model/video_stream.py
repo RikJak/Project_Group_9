fb9328e65aa2c939bded410d93f78f6a74b2a52e
jakmol@ug.kth.se <MmR-PZ-Q>
2019-04-09 15:16:21 +0200

    video streaming fixing

--- model/video_stream.py
+++ model/video_stream.py
@@ -20,12 +20,12 @@ def start_stream(client_ip,port):
         #     return render_template('index.html')
     def gen(camera):
         """Video streaming generator function."""
-    while True:
+        while True:
             frame = camera.get_frame()
             yield (b'--frame\r\n'+b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-            @app.route('/video_feed')
-            def video_feed():
-                """Video streaming route. Put this in the src attribute of an img tag."""
-                return Response(gen(Camera()), mimetype='multipart/x-mixed-replace; boundary=frame')
-            if __name__ == '__main__':
-                app.run(host=SERVER_IP, port = PORT, debug=True, threaded=True)
+    @app.route('/video_feed')
+    def video_feed():
+            """Video streaming route. Put this in the src attribute of an img tag."""
+            return Response(gen(Camera()), mimetype='multipart/x-mixed-replace; boundary=frame')
+    if __name__ == '__main__':
+        app.run(host=SERVER_IP, port = PORT, debug=True, threaded=True)
