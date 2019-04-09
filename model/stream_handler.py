b870fd5a420309f9b7828dac1c7fbcf39ef716e1
jakmol@ug.kth.se <MmR-PZ-Q>
2019-04-09 20:35:31 +0200

    testing variable send

--- model/stream_handler.py
+++ model/stream_handler.py
@@ -26,8 +26,9 @@ class StreamHandler:
         valid = self.validate.validate_user(email,api_key)
         valid = True
         if (valid):
-
-            os.system("sudo python3 /home/Project_Group_9/model/video_stream.py &")
+            start_command = f"sudo python3 /home/Project_Group_9/model/video_stream.py {client_ip} {PORT} &"
+            os.system(start_command)
+            # os.system("sudo python3 /home/Project_Group_9/model/video_stream.py &")
             # video_stream.start_stream(client_ip,PORT)
             return json.dumps({'port':PORT, 'server_ip': SERVER_IP})
         return '', 403
