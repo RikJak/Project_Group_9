3c01f41938c2a578c4588ae8a3795a89f595e183
jakmol@ug.kth.se <MmR-PZ-Q>
2019-04-09 14:49:05 +0200

    restricted access to stream with ip

--- view/view.py
+++ view/view.py
@@ -14,7 +14,7 @@ def request_handler():
    if request.method == 'POST':
       email = request.form.get('email')
       api_key = request.form.get('api_key')
-      client_ip = request.remote_addr
+      client_ip = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
       # client_ip = request.environ.get('REMOTE_ADDR')
       controller.set_up_stream(email,api_key,client_ip)  
       # return redirect(url_for('success',name = user))
