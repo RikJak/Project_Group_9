bbd1506c32cf1473a5d1803bbeefb4d5ef7527fa
jakmol@ug.kth.se <MmR-PZ-Q>
2019-04-09 20:47:41 +0200

    testing variable send

--- model/video_stream.py
+++ model/video_stream.py
@@ -10,7 +10,7 @@ from flask import Flask, render_template, Response
 
 # Raspberry Pi camera module (requires picamera package)
 from camera_pi import Camera
-# print(sys.argv)
+print(sys.argv)
 # client_ip = sys.argv[1]
 # port = sys.argv[2]
 
