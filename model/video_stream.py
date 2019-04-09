69e03951ad04efe00ea324f4ffa511596c78a3ff
jakmol@ug.kth.se <MmR-PZ-Q>
2019-04-09 21:06:35 +0200

    testing variable send

--- model/video_stream.py
+++ model/video_stream.py
@@ -13,7 +13,7 @@ from camera_pi import Camera
 print(sys.argv)
 print(len(sys.argv))
 arguments = sys.argv
-print(argument[0])
+print(arguments[0])
 print(arguments[1])
 # client_ip = sys.argv[1]
 # port = sys.argv[2]
