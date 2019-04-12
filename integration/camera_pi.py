9d28b93d4481e2077feda20618d12c6a871a4a66
jakmol@ug.kth.se <MmR-PZ-Q>
2019-04-12 12:34:54 +0200

    camera res

--- integration/camera_pi.py
+++ integration/camera_pi.py
@@ -8,7 +8,7 @@ class Camera(object):
     thread = None  # background thread that reads frames from camera
     frame = None  # current frame is stored here by background thread
     last_access = 0  # time of last client access to the camera
-    def __init__(res_x, res_y):
+    def __init__(self,res_x, res_y):
         self.res_x = res_x
         self.res_y = res_y
 
