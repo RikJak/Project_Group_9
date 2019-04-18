fa78de19c76adf4528d181da9a417225218a5727
jakmol@ug.kth.se <MmR-PZ-Q>
2019-04-18 14:31:26 +0200

    testing MAC

--- integration/device.py
+++ integration/device.py
@@ -1,6 +1,7 @@
 import uuid
 class Device:
     def get_MAC_address(self):
-        mac_address = (':'.join(['{:02x}'.format((uuid.getnode() >> elements) & 0xff) 
-        for elements in range(0,2*6,2)][::-1]))
-        return mac_address
+        # mac_address = (':'.join(['{:02x}'.format((uuid.getnode() >> elements) & 0xff) 
+        # for elements in range(0,2*6,2)][::-1]))
+        # return mac_address
+        return uuid.getnode()
