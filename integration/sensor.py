52caef43641dda56eeef429d76b9da33ae3e965f
jakmol@ug.kth.se <MmR-PZ-Q>
2019-04-24 13:46:04 +0200

    testing sensor

--- integration/sensor.py
+++ integration/sensor.py
@@ -2,6 +2,7 @@ import RPi.GPIO as GPIO
 import time
 class Sensor():
     def __init__(self):
+        GPIO.setmode(GPIO.BOARD)
         self.pir = 8
         GPIO.setup(self.pir,GPIO.IN)
         time.sleep(2)
