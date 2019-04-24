4a198e235b71862bb4a518f64570d869c72463cf
jakmol@ug.kth.se <MmR-PZ-Q>
2019-04-24 14:01:39 +0200

    testing sensor

--- integration/sensor.py
+++ integration/sensor.py
@@ -1,19 +1,18 @@
 import RPi.GPIO as GPIO
 import time
 import datetime
-class Sensor():
-    def __init__(self):
-        GPIO.setmode(GPIO.BOARD)
-        self.pir = 8
-        GPIO.setup(self.pir,GPIO.IN)
-        time.sleep(2)
+GPIO.setmode(GPIO.BCM)
+pir = 8
+GPIO.setup(pir,GPIO.IN)
+time.sleep(2)
+run_sensor()
     
-    def run_sensor(self):
-        try:
-            while True:
-                if GPIO.input(self.pir) == True:
-                    print(f"motion detected at: {datetime.datetime.now()}")
-                    time.sleep(5)
+def run_sensor():
+    try:
+        while True:
+            if GPIO.input(pir):
+                print(f"motion detected at: {datetime.datetime.now()}")
+                time.sleep(5)
 
-        finally:
-            GPIO.cleanup()
+    finally:
+        GPIO.cleanup()
