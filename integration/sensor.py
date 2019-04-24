c37d7b005b1b2f6a006b9170251337797fe2a2b6
jakmol@ug.kth.se <MmR-PZ-Q>
2019-04-24 14:03:59 +0200

    testing sensor

--- integration/sensor.py
+++ integration/sensor.py
@@ -1,11 +1,11 @@
 import RPi.GPIO as GPIO
 import time
 import datetime
-GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BOARD)
 pir = 8
 GPIO.setup(pir,GPIO.IN)
 time.sleep(2)
-    
+print("Sensor on")
 try:
     while True:
         if GPIO.input(pir):
