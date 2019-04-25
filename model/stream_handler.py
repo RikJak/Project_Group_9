858d80add2cfab6647cbb7e8aa835b46bc15f7b3
jakmol@ug.kth.se <MmR-PZ-Q>
2019-04-25 14:34:39 +0200

    sleep before response

--- model/stream_handler.py
+++ model/stream_handler.py
@@ -3,6 +3,7 @@ import sys
 FILE_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(f"{FILE_DIR}/../integration")
 
+import time
 from validate import Validate
 import json
 import requests
@@ -33,6 +34,7 @@ class StreamHandler:
             # run("/home/Project_Group_9/model/video_stream.py", client_ip, PORT)
             start_command = f"sudo python3 /home/Project_Group_9/model/video_stream.py {client_ip} {PORT} {server_IP} &"
             os.system(start_command)
+            time.sleep(2)
             return json.dumps({'port':PORT, 'server_ip': server_IP})
         return '', 403
 
