fed8138c6bd98cdf065cea7dcac1d17283172bdf
jakmol@ug.kth.se <MmR-PZ-Q>
2019-04-25 09:40:58 +0200

    sensor.py done

--- sensor.py
+++ sensor.py
@@ -1,26 +1,16 @@
-# import RPi.GPIO as GPIO
-# import time
-# import datetime
-print('Start of sensor service')
 import requests
 import threading
 from flask_cors import CORS, cross_origin
 from flask import Flask, render_template, Response, request, abort
 import os
 import sys
+import json
 FILE_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(f"{FILE_DIR}/integration")
 from sensor_pi import SensorPi
 
-#Setup of sensor
-# GPIO.setmode(GPIO.BOARD)
-# pir = 8
-# GPIO.setup(pir,GPIO.IN)
-# time.sleep(2)
-
 #Variables
 number_of_args=len(sys.argv)
-print(number_of_args)
 if  number_of_args >1:
     server_IP= sys.argv[3]
     PORT = sys.argv[2]
@@ -43,11 +33,13 @@ def sensor_on():
     global sensor_pi
     sensor_pi = SensorPi(server_IP)
     sensor_pi.sensor_on()
+    return json.dumps({'msg':'Sensor turned on'})
 
 @app.route('/sensor_off', methods = ['POST'])
 def sensor_off():
     global sensor_pi
     sensor_pi.sensor_off()
+    return json.dumps({'msg':'Sensor turned off'})
 
 if __name__ == '__main__':
     app.run(host=server_IP, port=PORT, debug=True, threaded=True)
