847c077f5ca240081581398bf225f7d6f6abd10a
rikjak <=>
2019-05-02 09:45:34 +0200

    broke validate

--- integration/validate.py
+++ integration/validate.py
@@ -2,9 +2,10 @@
 
 import requests
 import json
-from config import Config
-config = Config()
-server_address = config.get_webserver_IP() #'https://g9.apic.eu-gb.mybluemix.net/'
+#from config import Config
+#config = Config()
+#server_address = config.get_webserver_IP() 
+server_address = 'https://g9.apic.eu-gb.mybluemix.net/'
 class Validate():
 
 
@@ -12,15 +13,16 @@ class Validate():
         """This method contacts the cloud server to verify that the user is valid. If status code 200 is returned the user is valid, and the function returns true
         @Rikard
         """
-        content = {'email':email,'api_key':api_key}
-        content = json.dumps(content)
-        headers = {"Content-Type":"application/json"}
-        r = requests.post(f'{server_address}/api/user/validate_user', data = content,headers = headers,verify=False)
-        msg = r.json()
+        # content = {'email':email,'api_key':api_key}
+        # content = json.dumps(content)
+        # headers = {"Content-Type":"application/json"}
+        # r = requests.post(f'{server_address}/api/user/validate_user', data = content,headers = headers,verify=False)
+        # msg = r.text
+        # print(msg)
 
-        if(msg['result']=='true'):
-            return True
+        # if(msg['result']=='true'):
+        #     return True
 
 
-        return False #Change when it's working
+        return True #Change when it's working
     
--- integration/webserver_connection.py
+++ integration/webserver_connection.py
@@ -9,15 +9,15 @@ class WebserverConnection:
         self.webserver_address= config.get_webserver_IP()
 
     def change_IP(self,server_IP,MAC):
-        # content = {'server_IP':server_IP,'MAC_address':MAC}
-        # content = json.dumps(content)
-        # headers = {"Content-Type":"application/json"}
-        # address will be given by the api
-        # r = requests.post(f"{self.websever_address}/api/something", data = content,headers = headers,verify=False) # needs to be changed!
-        # if(r.status_code == 200):
+        content = {'server_IP':server_IP,'MAC_address':MAC}
+        content = json.dumps(content)
+        headers = {"Content-Type":"application/json"}
+        address will be given by the api
+        r = requests.post(f"{self.websever_address}/api/camera/update_ip", data = content,headers = headers,verify=False)
+        if(r.status_code == 200):
         return True
     
     def send_photo(self,filename):
-        # files = {'media' : open(filename,'rb')}
-        # r = requests.post(f"{self.webserver_address}/api/something", files = files) # url needs to be changed!
+        files = {'media' : open(filename,'rb')}
+        r = requests.post(f"{self.webserver_address}//api/user/notify", files = files) # url needs to be changed!
         return True
