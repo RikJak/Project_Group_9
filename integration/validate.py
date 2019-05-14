3889481079be57cb455538f1519f4bc7175a8c9f
rikjak <=>
2019-05-14 14:17:47 +0200

    configfile

--- integration/validate.py
+++ integration/validate.py
@@ -6,8 +6,8 @@ import os,sys
 
 FILE_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(f"{FILE_DIR}/../")
-from .configFile import Config
-config = Config()
+import configFile
+config = configFile.Config()
 server_address = config.get_webserver_IP() 
 #server_address = 'http://g9.apic.eu-gb.mybluemix.net/'
 class Validate():
